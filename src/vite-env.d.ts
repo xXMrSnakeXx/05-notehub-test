33e7c2268c78dc3c51bd82e8383b95ae7ae6689d
Eliza <elizavetasmolka@gmail.com>
2025-08-15 17:29:50 +0200

    .

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});
